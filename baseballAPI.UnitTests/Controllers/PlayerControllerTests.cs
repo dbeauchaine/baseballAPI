cc6ade7dd8d1ddae7d916ec1b1701c794302bde1
Dan Beauchaine <danbeauchaine@gmail.com>
2019-10-22 21:25:38 -0700

    Initialized Test environment
    Added a test project to the solution and cleaned up the auto-generated code that wasn't useful.

---
+++ baseballAPI.UnitTests/Controllers/PlayerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using baseballAPI.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baseballAPI.UnitTests.Controllers
+{
+    [TestFixture]
+    [Category("Unit")]
+    public class PlayerControllerTests
+    {
+        [Test]
+        public void PlayerControllerExists() 
+        {
+            var test = new PlayerController();
+        }
+
+        [Test]
+        public void ReturnsPlayerID()
+        {
+            var firstname = "first";
+            var lastname = "last";
+            var controller = new PlayerController();
+            var playerID = controller.GetPlayerID(firstname, lastname);
+        }
+    }
+}
---
+++ baseballAPI/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace baseballAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        public PlayerController()
+        {
+
+        }
+
+        [HttpGet]
+        public string GetPlayerID(string firstname, string lastname)
+        {
+            return null;
+        }
+    }
+}
