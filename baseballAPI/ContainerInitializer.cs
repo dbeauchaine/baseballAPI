cc845d0643784b8f2d7a8e1d06ebaa5b8051c39f
Dan Beauchaine <danbeauchaine@gmail.com>
2019-10-26 15:49:39 -0700

    Removed Container Initialization to a seperate class

---
+++ baseballAPI/ContainerInitializer.cs
@@ -0,0 +1,20 @@
+﻿using baseballAPI.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace baseballAPI
+{
+    public class ContainerInitializer
+    {
+        IServiceCollection _services;
+        public ContainerInitializer(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public void Execute()
+        {
+            _services.AddControllers();
+            _services.AddTransient<IPlayerService, PlayerService>();
+        }
+    }
+}
---
+++ baseballAPI/Services/PlayerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace baseballAPI.Services
+{
+    public class PlayerService : IPlayerService
+    {
+        public string GetPlayerId(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
