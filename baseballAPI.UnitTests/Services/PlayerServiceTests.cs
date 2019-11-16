4ccc0f889740d32860458e3d8269e0fc1a045395
Dan Beauchaine <danbeauchaine@gmail.com>
2019-11-15 16:45:40 -0800

    Added Test to PlayerService for get with multiple entries to a database

--- baseballAPI.UnitTests/Services/PlayerServiceTests.cs
+++ baseballAPI.UnitTests/Services/PlayerServiceTests.cs
@@ -21,14 +21,14 @@ namespace BaseballAPI.UnitTests.Controllers
         public void SetUp()
         {
             _options = new DbContextOptionsBuilder<BaseballDBContext>()
-                .UseInMemoryDatabase(databaseName: "Get_returns_id")
+                .UseInMemoryDatabase(databaseName: "PlayerServiceTests")
                 .Options;
             _database = new BaseballDBContext(_options);
             _service = new PlayerService(_database);
         }
 
         [Test]
-        public void GetReturnsId()
+        public void GetReturnsIdOneEntry()
         {
             var firstName = "first";
             var lastName = "last";
@@ -41,6 +41,31 @@ namespace BaseballAPI.UnitTests.Controllers
                 PlayerId = expectedPlayerId
             });
            
+            _database.SaveChanges();
+
+            var playerId = _service.GetPlayerId(firstName, lastName);
+
+            Assert.That(playerId, Is.EqualTo(expectedPlayerId));
+        }
+
+        [Test]
+        public void GetReturnsIdMultipleEntries()
+        {
+            //assumes previous add to database from GetReturnsIdOneEntry
+            var firstName = "first";
+            var lastName = "last";
+            var expectedPlayerId = "something";
+            var anotherFirstName = "anotherFirst";
+            var anotherLastName = "anotherLast";
+            var anotherPlayerId = "somethingElse";
+
+            _database.Add(new People
+            {
+                NameFirst = anotherFirstName,
+                NameLast = anotherLastName,
+                PlayerId = anotherPlayerId
+            });
+
             _database.SaveChanges();
 
             var playerId = _service.GetPlayerId(firstName, lastName);
