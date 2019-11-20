888043417437697ac68db317077d7e5230900929
Dan Beauchaine <danbeauchaine@gmail.com>
2019-11-19 20:31:54 -0800

    Enabled CORS

--- baseballAPI/Startup.cs
+++ baseballAPI/Startup.cs
@@ -13,11 +13,21 @@ namespace BaseballAPI
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("*");
+                });
+            });
+
             ContainerInitializer initializer = new ContainerInitializer(services);
             initializer.Execute();
         }
@@ -29,6 +39,8 @@ namespace BaseballAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
 
